99cfa5dd846e4069b552cee99e9153cd335ac21c
johnnyFa <johnnyfagundes9@gmail.com>
2024-10-14 20:33:21 -0300

    feat: create entities

---
+++ src/main/java/com/fagundes/springboot/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.fagundes.springboot.domain.address;
+
+import com.fagundes.springboot.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
